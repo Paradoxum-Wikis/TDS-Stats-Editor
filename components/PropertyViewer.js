90daeecae0a57eadce368f3f8e283401dc03ab72
tofu <57356716+t7ru@users.noreply.github.com>
2025-02-17 16:51:34 -0500

    Update

--- components/PropertyViewer.js
+++ components/PropertyViewer.js
@@ -16,6 +16,7 @@ export default class PropertyViewer {
             'Coverage',
             'BossPotential',
             'LimitBossPotential',
+            'BossValue'
         ];
         this.baseProperties = [
             'Damage',
