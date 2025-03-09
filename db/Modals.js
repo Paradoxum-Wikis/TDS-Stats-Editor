01cafce37406ddef053b942fdcdffa026322e1d1
tofu <57356716+t7ru@users.noreply.github.com>
2025-03-08 23:25:48 -0500

    feat: help and guidelines

---
+++ db/Modals.js
@@ -0,0 +1,21 @@
+// Help modal tab switching
+document.addEventListener('DOMContentLoaded', function() {
+    const helpBrowser = document.getElementById('helpBrowser');
+    const helpUpload = document.getElementById('helpUpload');
+    const browserContent = document.getElementById('browserHelpContent');
+    const uploadContent = document.getElementById('uploadHelpContent');
+        
+    helpBrowser.addEventListener('change', function() {
+        if (this.checked) {
+                browserContent.classList.remove('d-none');
+                uploadContent.classList.add('d-none');
+        }
+    });
+        
+    helpUpload.addEventListener('change', function() {
+        if (this.checked) {
+            uploadContent.classList.remove('d-none');
+            browserContent.classList.add('d-none');
+        }
+    });
+});
