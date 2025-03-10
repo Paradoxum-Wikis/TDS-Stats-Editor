4be01592252ecd3da59a381fa066c6f7ce0117d9
tofu <57356716+t7ru@users.noreply.github.com>
2025-03-10 19:00:13 -0400

    style: moved update log date

--- UpdateLog.js
+++ UpdateLog.js
@@ -91,14 +91,16 @@ class UpdateLog {
                             ${scope ? `<span class="text-muted">(${scope})</span> ` : ''}
                             ${description}
                         </h5>
-                        <small class="text-muted">${formattedDate}</small>
                     </div>
                     <p class="mb-1 text-muted">by ${commit.commit.author.name}</p>
-                    <small>
-                        <a href="${commit.html_url}" target="_blank" class="text-info">
-                            View on GitHub
-                        </a>
-                    </small>
+                    <div class="d-flex w-100 justify-content-between">
+                        <small>
+                            <a href="${commit.html_url}" target="_blank" class="text-info">
+                                View on GitHub
+                            </a>
+                        </small>
+                        <small class="text-muted">${formattedDate}</small>
+                    </div>
                 </div>
             `;
         });
