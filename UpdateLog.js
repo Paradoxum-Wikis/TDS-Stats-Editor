0ec3f69122fa692018599fe3f7d33fae6548a659
tofu <57356716+t7ru@users.noreply.github.com>
2025-03-10 20:43:04 -0400

    style: clarity

--- UpdateLog.js
+++ UpdateLog.js
@@ -96,7 +96,7 @@ class UpdateLog {
                     <div class="d-flex w-100 justify-content-between">
                         <small>
                             <a href="${commit.html_url}" target="_blank" class="text-info">
-                                View on GitHub
+                                View more on GitHub
                             </a>
                         </small>
                         <small class="text-muted">${formattedDate}</small>
