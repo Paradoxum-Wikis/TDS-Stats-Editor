647d273fb66b25ad3f3e1989727c7952d411acd0
Benjamin <gabonnie1234@gmail.com>
2025-06-14 13:54:17 -0400

    fix: old data isn't merged with new data

--- TowerComponents/TowerManager.js
+++ TowerComponents/TowerManager.js
@@ -19,12 +19,80 @@ class TowerManager {
     const loadedStatic = JSON.parse(JSON.stringify(TowerData));
 
     for (let [key, value] of Object.entries(loadedStatic)) {
-      if (key in loadedLocal) continue;
-
-      loadedLocal[key] = value;
+      if (key in loadedLocal) {
+        loadedLocal[key] = this.mergeDefaultProperties(loadedLocal[key], value);
+      } else {
+        loadedLocal[key] = value;
+      }
     }
 
     return loadedLocal;
+  }
+
+  // merge default properties while preserving user customizations
+  mergeDefaultProperties(localTowerData, defaultTowerData) {
+    const merged = JSON.parse(JSON.stringify(localTowerData));
+    
+    // For each skin variant (Default, Golden, etc.)
+    for (const [skinName, skinData] of Object.entries(defaultTowerData)) {
+      if (!merged[skinName]) {
+        merged[skinName] = JSON.parse(JSON.stringify(skinData));
+        continue;
+      }
+      
+      // merge Defaults section
+      if (skinData.Defaults && merged[skinName].Defaults) {
+        for (const [prop, value] of Object.entries(skinData.Defaults)) {
+          if (!(prop in merged[skinName].Defaults)) {
+            merged[skinName].Defaults[prop] = value;
+          }
+        }
+        
+        // merge new attributes
+        if (skinData.Defaults.Attributes && merged[skinName].Defaults.Attributes) {
+          for (const [attr, value] of Object.entries(skinData.Defaults.Attributes)) {
+            if (!(attr in merged[skinName].Defaults.Attributes)) {
+              merged[skinName].Defaults.Attributes[attr] = value;
+            }
+          }
+        }
+      }
+      
+      // Merge new upgrades
+      if (skinData.Upgrades && Array.isArray(skinData.Upgrades)) {
+        if (!merged[skinName].Upgrades) {
+          merged[skinName].Upgrades = [];
+        }
+        
+        // If default has more upgrades than local - add the new ones
+        if (skinData.Upgrades.length > merged[skinName].Upgrades.length) {
+          for (let i = merged[skinName].Upgrades.length; i < skinData.Upgrades.length; i++) {
+            merged[skinName].Upgrades[i] = JSON.parse(JSON.stringify(skinData.Upgrades[i]));
+          }
+        }
+        
+        // update existing upgrades with new properties
+        for (let i = 0; i < Math.min(merged[skinName].Upgrades.length, skinData.Upgrades.length); i++) {
+          if (skinData.Upgrades[i].Stats?.Attributes) {
+            if (!merged[skinName].Upgrades[i].Stats) {
+              merged[skinName].Upgrades[i].Stats = {};
+            }
+            if (!merged[skinName].Upgrades[i].Stats.Attributes) {
+              merged[skinName].Upgrades[i].Stats.Attributes = {};
+            }
+            
+            // this adds new attributes to upgrade levels
+            for (const [attr, value] of Object.entries(skinData.Upgrades[i].Stats.Attributes)) {
+              if (!(attr in merged[skinName].Upgrades[i].Stats.Attributes)) {
+                merged[skinName].Upgrades[i].Stats.Attributes[attr] = value;
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    return merged;
   }
 
   getTowerData() {
