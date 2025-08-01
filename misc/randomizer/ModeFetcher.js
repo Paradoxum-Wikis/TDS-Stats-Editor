f272937cde393c37b598996794878ec830910835
Benjamin <gabonnie1234@gmail.com>
2025-07-31 22:24:03 -0400

    perf: gamemode images are now fetched instead

--- misc/randomizer/ModeFetcher.js
+++ misc/randomizer/ModeFetcher.js
@@ -11,10 +11,6 @@ class ModeFetcher {
       }
       const modesData = await response.json();
 
-      for (const mode of modesData) {
-        mode.imageUrl = `/htmlassets/${mode.imageFile}`;
-      }
-
       return modesData;
     } catch (error) {
       console.error("Error fetching or parsing mode data:", error);
