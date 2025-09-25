9e4dbebd4899f84f43053b00e909110953272c8c
Benjamin <gabonnie1234@gmail.com>
2025-09-24 20:43:38 -0400

    chore: updated stats to 1.75

--- Main.js
+++ Main.js
@@ -36,7 +36,7 @@ import "./components/News/HeadsUp.js";
 import * as bootstrap from "bootstrap";
 import CryptoJS from "crypto-js";
 
-const TDSVersion = "1.74.0"; // change GAME version number here
+const TDSVersion = "1.75.0"; // change GAME version number here
 
 window.clearUrlAndShowLanding = clearUrlAndShowLanding;
 window.bootstrap = bootstrap;
