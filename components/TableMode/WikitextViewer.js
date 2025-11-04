3e3a3761ff1fa5b22744230b5757d3c1ed6023bf
Benjamin <gabonnie1234@gmail.com>
2025-11-04 00:11:16 -0500

    refactor: replaced old custom wikitext formatter with wikistxr

--- components/TableMode/WikitextViewer.js
+++ components/TableMode/WikitextViewer.js
@@ -1,16 +1,17 @@
+import { WikitextHighlighter } from "wikistxr";
+
 class WikitextViewer {
   constructor() {
-    this.container = document.createElement("div");
+    this.container = document.querySelector("#wikitable-panel");
+    this.container.classList.add("wikitext-viewer");
+    this.highlighter = new WikitextHighlighter();
 
-    // this creates pre/code elements for syntax highlighting
-    this.pre = document.createElement("pre");
-    this.pre.className = "text-white mb-3 lmwikitable";
-
-    this.code = document.createElement("code");
-    this.code.className = "wikitable-code";
-
-    this.pre.appendChild(this.code);
-    this.container.appendChild(this.pre);
+    if (!document.getElementById("wikistxr-styles")) {
+      const styleTag = document.createElement("style");
+      styleTag.id = "wikistxr-styles";
+      styleTag.textContent = WikitextHighlighter.getDefaultStyles();
+      document.head.appendChild(styleTag);
+    }
   }
 
   getContainer() {
@@ -18,73 +19,12 @@ class WikitextViewer {
   }
 
   showWikitext(wikitext) {
-    // force reset everything to avoid conflicts
-    this.code.innerHTML = "";
+    this.container.innerHTML = "";
 
-    // apply them colors yuppers
-    const highlighted = this.highlightWikitext(wikitext);
-    this.code.innerHTML = highlighted;
+    const highlightedHtml = this.highlighter.highlight(wikitext);
+    this.container.innerHTML = highlightedHtml;
 
     return this;
-  }
-
-  highlightWikitext(text) {
-    // replace special characters before anything else
-    text = text
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;");
-
-    // highlight comments before other elements to avoid any interference
-    text = text.replace(
-      /(&lt;!--)([\s\S]*?)(&gt;)/g,
-      '<span class="wiki-comment">$1$2$3</span>',
-    );
-
-    // div tags
-    text = text.replace(/(&lt;div.*&gt;)/g, '<span class="wiki-div">$1</span>');
-    text = text.replace(/(&lt;\/div&gt;)/g, '<span class="wiki-div">$1</span>');
-
-    // table syntax
-    text = text.replace(
-      /(\{\||\|\})/g,
-      '<span class="table-delimiter">$1</span>',
-    );
-    text = text.replace(/(\|-)/g, '<span class="table-row">$1</span>');
-    text = text.replace(
-      /(\|\+\s*)('''[^']+''')/g,
-      '<span class="table-caption">$1</span><span class="table-caption-text">$2</span>',
-    );
-
-    // cell and header delimiters
-    text = text.replace(
-      /(!!)/g,
-      '<span class="table-header-delimiter">$1</span>',
-    );
-    text = text.replace(
-      /(\|\|)/g,
-      '<span class="table-cell-delimiter">$1</span>',
-    );
-
-    // 1st vertical bars
-    text = text.replace(
-      /^(\|)(?!-|\+)/gm,
-      '<span class="table-cell-delimiter">$1</span>',
-    );
-
-    // headers (must come after !! replacment)
-    text = text.replace(/^(!)/gm, '<span class="table-header">$1</span>');
-
-    // highlight formatting
-    text = text.replace(
-      /(''')(.*?)(''')/g,
-      '<span class="wiki-bold">$1</span><span class="wiki-bold-text">$2</span><span class="wiki-bold">$3</span>',
-    );
-
-    // add line breaks
-    text = text.replace(/\n/g, "<br>");
-
-    return text;
   }
 }
 
